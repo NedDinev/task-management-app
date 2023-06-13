8bf82ec74364971ef52a3dc3c424384ba3e2b77f
NedDinev <ned.dinev@gmail.com>
2023-06-13 22:28:37 +0300

    Delete unfinished tests

--- client/src/components/TaskMapper/TaskMapper.test.js
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom/extend-expect";
-import React from "react";
-import { render } from "@testing-library/react";
-import TaskMapper from "./TaskMapper";
-
-test("renders TaskMapper component without errors", () => {
-  render(<TaskMapper />);
-});
--- client/src/components/TaskWrapper/TaskWrapper.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import TaskWrapper from "./TaskWrapper";
-
-test("renders TaskWrapper component without errors", () => {
-  render(<TaskWrapper tasks={[]} />);
-});
