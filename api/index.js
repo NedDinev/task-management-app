ac8d5064257d17d77893a1e03f0da74c3deaae9a
NedDinev <ned.dinev@gmail.com>
2023-07-24 19:39:18 +0300

    Add cors obj

--- api/index.js
+++ api/index.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://task-management-app-client-lime.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL);
