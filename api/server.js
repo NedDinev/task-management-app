6a188444451e0b6d549f45bad30d81c10df356cf
NedDinev <ned.dinev@gmail.com>
2023-07-24 13:18:25 +0300

    Remove cors

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const path = require("node:path");
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
 
