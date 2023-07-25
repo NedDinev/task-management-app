26be36650ccc109e53728aaf5701382d5ef32fee
Nedelcho Dinev <ned.dinev@gmail.com>
2023-07-25 16:25:40 +0300

    Remove cors origin

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://task-management-app-client-lime.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
